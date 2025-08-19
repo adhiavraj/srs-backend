72a2e21932a95b37060222c9fd096fe8cfe83470
avbeingthewd24 <adhiavraj@gmail.com>
2025-08-19 15:20:44 +0530

    added cors for frontend hosting platform

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: "https://srs-pdf.pages.dev",
+  methods: ["GET", "POST"],
+}));
 
 // Routes
 app.use("/api", generateSRSRoute);
